5f5d7edbb39bbb5606fd3542f4604c2bb1db3ac1
Simon <radoszszymon@gmail.com>
2022-10-17 20:11:48 +0200

    fix: test lint-staged

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,29 @@
 module.exports = {
-    env: {
-        browser: true,
-        es2021: true,
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['plugin:react/recommended', 'standard-with-typescript'],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['./tsconfig.json'],
+  },
+  plugins: ['react'],
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    camelcase: 'error',
+    'spaced-comment': 'error',
+    quotes: ['error', 'single'],
+    'no-duplicate-imports': 'error',
+    '@typescript-eslint/strict-boolean-expressions': 0,
+    '@typescript-eslint/explicit-function-return-type': 0,
+  },
+  settings: {
+    react: {
+      version: 'detect',
     },
-    extends: ['plugin:react/recommended', 'standard-with-typescript'],
-    overrides: [],
-    parserOptions: {
-        ecmaVersion: 'latest',
-        sourceType: 'module',
-        project: ['./tsconfig.json'],
-    },
-    plugins: ['react'],
-    rules: {},
-    settings: {
-        react: {
-            version: 'detect',
-        },
-    },
-};
+  },
+  root: true,
+}
--- resources/js/components/Dashboard/Statistics/Statistics.tsx
+++ resources/js/components/Dashboard/Statistics/Statistics.tsx
@@ -1,43 +1,33 @@
-import * as React from "react";
-import SingleStat from "./SingleStat/SingleStat";
+import * as React from 'react'
+import SingleStat from './SingleStat/SingleStat'
 
-type userType = {
-    user: {
-        countSavedWordsOverall: number;
-        countSavedWordsLastWeek: number;
-        countSavedWordsToday: number;
-    };
-};
+interface userType {
+  user: {
+    countSavedWordsOverall: number
+    countSavedWordsLastWeek: number
+    countSavedWordsToday: number
+  }
+}
 
 const Statistics = ({ user }: userType) => {
-    return (
-        <div className="dashboard-statistics">
-            <SingleStat
-                number={
-                    user?.countSavedWordsOverall
-                        ? user?.countSavedWordsOverall
-                        : 0
-                }
-                text="zapisanych ogółem"
-            />
+  return (
+    <div className='dashboard-statistics'>
+      <SingleStat
+        number={user?.countSavedWordsOverall ? user?.countSavedWordsOverall : 0}
+        text='zapisanych ogółem'
+      />
 
-            <SingleStat
-                number={
-                    user?.countSavedWordsLastWeek
-                        ? user?.countSavedWordsLastWeek
-                        : 0
-                }
-                text="w tym tygodniu"
-            />
+      <SingleStat
+        number={user?.countSavedWordsLastWeek ? user?.countSavedWordsLastWeek : 0}
+        text='w tym tygodniu'
+      />
 
-            <SingleStat
-                number={
-                    user?.countSavedWordsToday ? user?.countSavedWordsToday : 0
-                }
-                text="zapisanych dzisiaj"
-            />
-        </div>
-    );
-};
+      <SingleStat
+        number={user?.countSavedWordsToday ? user?.countSavedWordsToday : 0}
+        text='zapisanych dzisiaj'
+      />
+    </div>
+  )
+}
 
-export default Statistics;
+export default Statistics
