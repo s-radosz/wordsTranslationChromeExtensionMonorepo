2268e2e29f21fa8ed5f320dee419ca148233c75b
Simon <radoszszymon@gmail.com>
2022-10-22 18:43:46 +0200

    test: cypress login test - log false

--- cypress/integration/LoginTests.spec.js
+++ cypress/integration/LoginTests.spec.js
@@ -2,8 +2,8 @@ describe('Login Tests', function () {
   it('Successfull login', function () {
     cy.visit('http://127.0.0.1:8000/login')
 
-    cy.get('#login-email-input').type(Cypress.env('user_name'))
-    cy.get('#login-password-input').type(Cypress.env('user_password'))
+    cy.get('#login-email-input').type(Cypress.env('user_name'), { log: false })
+    cy.get('#login-password-input').type(Cypress.env('user_password'), { log: false })
     cy.get('#login-submit').click()
 
     cy.url().should('contain', '/panel')
